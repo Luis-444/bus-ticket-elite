ce5597999f24228d8ded78d656be4056ddcb6957
Luis-44 <luismanuelmaganaloyo@gmail.com>
2024-02-27 13:16:41 -0600

    Primeros componentes

---
+++ src/components/Form.tsx
@@ -0,0 +1,12 @@
+import { Button } from 'flowbite-react';
+
+export default function Form() {
+  return (
+    <div className="flex flex-wrap gap-2">
+      <Button>Default</Button>
+      <Button>Default</Button>
+      <Button>Default</Button>
+      <Button>Default</Button>
+    </div>
+  );
+}
