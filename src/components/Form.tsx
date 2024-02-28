9a6e240336aba28de3afb590d2e7878b2e1a224a
Luis-44 <luismanuelmaganaloyo@gmail.com>
2024-02-28 13:42:00 -0600

    formulario de compra

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,55 @@
+import { Label, TextInput } from 'flowbite-react';
+import { Select } from 'flowbite-react';
 import { Button } from 'flowbite-react';
 
 export default function Form() {
   return (
-    <div className="flex flex-wrap gap-2">
-      <Button>Default</Button>
-      <Button>Default</Button>
-      <Button>Default</Button>
-      <Button>Default</Button>
+    <div className="flex max-w-md flex-col gap-4">
+      <div>
+        <TextInput id="base" type="date" sizing="md" />
+      </div>
+      <div className="flex">
+        <div className="w-3/4">
+          <div className="max-w-md">
+            <div className="mb-2 block">
+              <Label htmlFor="small" value="Selecciona la ciudad" />
+            </div>
+            <Select id="countries" required>
+              <option>Matamoros</option>
+              <option>Valle</option>
+            </Select>
+          </div>
+        </div>
+        <div className="w1/4">
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="large" value="Precio Unitario" />
+            </div>
+            <TextInput id="large" type="number" sizing="md" value={'10'} disabled />
+          </div>
+        </div>
+      </div>
+      <div className="flex">
+        <div className="w-3/4">
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="small" value="Ingresa la cantidad de boletos" />
+            </div>
+            <TextInput id="small" type="number" sizing="md" />
+          </div>
+        </div>
+        <div className="w1/4">
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="large" value="Precio Total a pagar" />
+            </div>
+            <TextInput id="large" type="number" sizing="md" value={'0'} disabled />
+          </div>
+        </div>
+      </div>
+      <div className="mb-2 block">
+        <Button className="bg-red-500">Comprar</Button>
+      </div>
     </div>
   );
 }
