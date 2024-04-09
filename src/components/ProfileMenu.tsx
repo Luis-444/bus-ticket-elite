8957340fd29b3e50bf5822d8ef475000ab376381
unknown <unova.x98@gmail.com>
2024-04-09 17:02:15 -0500

    Reparando los errores de Luis

--- src/components/ProfileMenu.tsx
+++ src/components/ProfileMenu.tsx
@@ -1,10 +1,10 @@
 export default function ProfileMenu() {
     return (
-        <ul class="grid grid-cols-2 md:grid-cols-4 w-full border-collapse bg-secondary shadow-md">
-            <li class="w-full"><a class="text-primary-hover p-2 hover:text-secondary hover:bg-primary-hover transition-all duration-300 w-full block text-center" href="/profile">Mi perfil</a></li>
-            <li class="w-full"><a class="text-primary-hover p-2 hover:text-secondary hover:bg-primary-hover transition-all duration-300 w-full block text-center" href="/profile">Mis Boletos</a></li>
-            <li class="w-full"><a class="text-primary-hover p-2 hover:text-secondary hover:bg-primary-hover transition-all duration-300 w-full block text-center" href="/profile">Metodos de pago</a></li>
-            <li class="w-full"><a class="text-primary-hover p-2 hover:text-secondary hover:bg-primary-hover transition-all duration-300 w-full block text-center" href="/profile">Configuración</a></li>
+        <ul className="grid grid-cols-2 md:grid-cols-4 w-full border-collapse bg-secondary shadow-md">
+            <li className="w-full"><a className="text-primary-hover p-2 hover:text-secondary hover:bg-primary-hover transition-all duration-300 w-full block text-center" href="/profile">Mi perfil</a></li>
+            <li className="w-full"><a className="text-primary-hover p-2 hover:text-secondary hover:bg-primary-hover transition-all duration-300 w-full block text-center" href="/profile">Mis Boletos</a></li>
+            <li className="w-full"><a className="text-primary-hover p-2 hover:text-secondary hover:bg-primary-hover transition-all duration-300 w-full block text-center" href="/profile">Metodos de pago</a></li>
+            <li className="w-full"><a className="text-primary-hover p-2 hover:text-secondary hover:bg-primary-hover transition-all duration-300 w-full block text-center" href="/profile">Configuración</a></li>
         </ul>
     );
 }
